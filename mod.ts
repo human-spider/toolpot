8402c953880f191dbfa1315d1194581a696eb938
Mykhailo Gryshchenko <mykhailogryshchenko@cloud.upwork.com>
2025-03-14 01:24:51 +0200

    add models list endpoint

--- mod.ts
+++ mod.ts
@@ -31,9 +31,28 @@ export type ToolpotConfig = {
   agents: Record<string, ToolpotAgentConfig>
 }
 
+const authMiddlware = (apiKeys: string[]) => (req: Request, res: Response, next: VoidFunction) => {
+  const authorization = req.get('authorization')
+  const apiKey =
+    authorization?.split('Bearer ')[1] ||
+    req.headers['x-api-key'] ||
+    req.headers['api-key']
+
+  if (!apiKey) {
+    return res.status(400).send('Missing api-key, x-api-key, or authorization header')
+  }
+
+  if (!apiKeys.includes(apiKey)) {
+    return res.status(403).send('Invalid API key')
+  }
+
+  next()
+}
+
 export const toolpot = (config: ToolpotConfig): void => {
   const app = express()
   app.use(cors())
+
   app.use(express.json())
 
   let generatedApiKey: string
@@ -47,25 +66,20 @@ export const toolpot = (config: ToolpotConfig): void => {
     apiKeys.push(generatedApiKey)
   }
 
+  app.use('/v1/*', authMiddlware(apiKeys))
+
   app.get('/', (_: Request, res: Response) => {
     res.send('OK')
   })
 
+  app.get('/v1/models', (_: Request, res: Response) => {
+    res.json({
+      data: Object.keys(config.agents).map(id => ({ id, object: 'model' })),
+      object: 'list',
+    })
+  })
+
   app.post('/v1/chat/completions', async (req: Request, res: Response) => {
-    const authorization = req.get('authorization')
-    const apiKey =
-      authorization?.split('Bearer ')[1] ||
-      req.headers['x-api-key'] ||
-      req.headers['api-key']
-
-    if (!apiKey) {
-      return res.status(400).send('Missing api-key, x-api-key, or authorization header')
-    }
-
-    if (!apiKeys.includes(apiKey)) {
-      return res.status(403).send('Invalid API key')
-    }
-
     try {
       await handleRequest(req, res, config)
     } catch (error) {
